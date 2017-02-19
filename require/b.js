81b6a7f41cb83a46192528ee98e1ce4afbb2333d
Lcj-space <1367546524@qq.com>
2017-02-19 15:40:53 +0800

    require.js了解

---
+++ require/b.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 13675 on 2017/2/19.
+ */
+define(function () {
+    function isArray(arr) {
+        return arr instanceof Array;
+    }
+    return isArray;
+});
---
+++ require/test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by 13675 on 2017/2/19.
+ */
+require(['a'],function (arraySort) {
+    var arr='123879';
+    // var arr= +
+    console.log(arraySort(arr));
+});
