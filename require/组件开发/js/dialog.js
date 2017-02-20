01aee1aa8a26ed399353d7022c8bf1c09a079252
Lcj-space <1367546524@qq.com>
2017-02-20 15:18:28 +0800

    组件

--- require/组件开发/js/dialog.js
+++ require/组件开发/js/dialog.js
@@ -2,40 +2,76 @@
  * Created by 13675 on 2017/2/19.
  */
 define(['jquery'], function ($) {
-    return {
-        // this就是retrun的对象
-        open: function (options) {
-            var sitting={
-                width: 500,
-                height: 400,
-                title:"这是标题"
-            };
-            $.extend(sitting,options);
-            var that = this;
-            this.$container = $('<div class="demo-container">');
-            var $mask = $('<div class="demo-mask"></div>').on('click', function () {
-                that.close();
-            });
-            var $content = $('<div class="demo-content">').css({
-                width:sitting.width,
-                height:sitting.height
-            });
-            var $demoTitle = $('<div class="demo-title">');
-            var $title = $('<span class="title">'+sitting.title +'</span>');
-            var $close = $('<span class="close">[X]</span>').on('click',function () {
-                that.close();
-            });
-            var $main = $('<div class="demo-main"></div>').load(sitting.url);
-            $demoTitle.append($title).append($close);
-            $content.append($demoTitle).append($main);
-            this.$container.append($mask).append($content);
-            $(document.body).append(this.$container);
-        },
+    // 用类的方法编写规避重复点击出现只能关一个窗口
+    function Dialog() {
 
-        close: function () {
-            this.$container.remove();
-        }
     }
+    Dialog.prototype.open = function (options) {
+                var sitting = {
+                    width: 500,
+                    height: 400,
+                    title: "这是标题"
+                };
+                $.extend(sitting, options);
+                var that = this;
+                this.$container = $('<div class="demo-container">');
+                var $mask = $('<div class="demo-mask"></div>').on('click', function () {
+                    that.close();
+                });
+                var $content = $('<div class="demo-content">').css({
+                    width: sitting.width,
+                    height: sitting.height
+                });
+                var $demoTitle = $('<div class="demo-title">');
+                var $title = $('<span class="title">' + sitting.title + '</span>');
+                var $close = $('<span class="close">[X]</span>').on('click', function () {
+                    that.close();
+                });
+                var $main = $('<div class="demo-main"></div>').load(sitting.url);
+                $demoTitle.append($title).append($close);
+                $content.append($demoTitle).append($main);
+                this.$container.append($mask).append($content);
+                $(document.body).append(this.$container);
+            }
+
+    Dialog.prototype.close =function () {
+        this.$container.remove();
+    };
+    return Dialog;
+    // return {
+    //     // this就是retrun的对象
+    //     open: function (options) {
+    //         var sitting = {
+    //             width: 500,
+    //             height: 400,
+    //             title: "这是标题"
+    //         };
+    //         $.extend(sitting, options);
+    //         var that = this;
+    //         this.$container = $('<div class="demo-container">');
+    //         var $mask = $('<div class="demo-mask"></div>').on('click', function () {
+    //             that.close();
+    //         });
+    //         var $content = $('<div class="demo-content">').css({
+    //             width: sitting.width,
+    //             height: sitting.height
+    //         });
+    //         var $demoTitle = $('<div class="demo-title">');
+    //         var $title = $('<span class="title">' + sitting.title + '</span>');
+    //         var $close = $('<span class="close">[X]</span>').on('click', function () {
+    //             that.close();
+    //         });
+    //         var $main = $('<div class="demo-main"></div>').load(sitting.url);
+    //         $demoTitle.append($title).append($close);
+    //         $content.append($demoTitle).append($main);
+    //         this.$container.append($mask).append($content);
+    //         $(document.body).append(this.$container);
+    //     },
+    //
+    //     close: function () {
+    //         this.$container.remove();
+    //     }
+    // }
 
 
 });
--- require/组件开发/js/index.js
+++ require/组件开发/js/index.js
@@ -3,14 +3,30 @@
  */
 require(['jquery', 'dialog'], function ($, Dialog) {
     $('#btn1').on('click', function () {
-        Dialog.open({
+        var dialog = new Dialog();
+        // $.prototype.aa = dialog;
+        dialog.open({
             width: 400,
             height: 500,
-            title:"登录",
-            url:('./form.html')
+            title: "登录",
+            url: ('./form.html'),
+            flag:1
         });
+        return dialog;
     });
     $('#btn2').on('click', function () {
-        Dialog.close();
-    })
+          Dialog.close();
+    });
+    // $('#btn1').on('click', function () {
+    //     Dialog.open({
+    //         width: 400,
+    //         height: 500,
+    //         title: "登录",
+    //         url: ('./form.html'),
+    //         flag:1
+    //     });
+    // });
+    // $('#btn2').on('click', function () {
+    //     Dialog.close();
+    // })
 });
