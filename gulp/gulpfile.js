cc61b12889a16d39610c3f623d9440c9f01210c9
Lcj-space <1367546524@qq.com>
2017-03-12 22:52:29 +0800

    gulp 的各个功能的应用

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,6 +2,9 @@
  * Created by 13675 on 2017/3/12.
  */
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
 // gulp.task('hell0',function () {
 //
 // });
@@ -15,7 +18,7 @@ gulp.task('test2', function () {
 gulp.task('default', ['test', 'test2']);
 
 gulp.task('task1', function () {
-    gulp.src('index.html').pipe(gulp.dest('dest'));
+    gulp.src('index.html').pipe(gulp.dest('dest')).pipe(connect.reload());
 });
 gulp.task('watch', function () {
     gulp.watch('index.html', ['task1']);
@@ -23,6 +26,14 @@ gulp.task('watch', function () {
 gulp.task('copy', function () {
     gulp.src('img/*.png').pipe(gulp.dest('dest/imgs'));
 });
-gulp.task('severs',function () {
-
+gulp.task('sass',function () {
+     gulp.src('sass/*.scss').pipe(sass()).pipe(uglify()).pipe(gulp.dest('dest/sass'));
 });
+
+gulp.task('severs',function () {
+    connect.server({
+       root:'dest',
+        livereload:true
+    })
+});
+gulp.task('a',['watch','severs']);
